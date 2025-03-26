bec06234de1932d73f136bc6315609ae06770e17
Your Name <your.email@example.com>
2025-03-26 15:50:40 +0530

    Create Model & Repository

---
+++ src/main/java/com/skill_sharing_platform/skill_sharing_platform/Model/User.java
@@ -0,0 +1,29 @@
+package com.skill_sharing_platform.skill_sharing_platform.Model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String password;
+    private String profilePicture;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Post> posts;
+}
