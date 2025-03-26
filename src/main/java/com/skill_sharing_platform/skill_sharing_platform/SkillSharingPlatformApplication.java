b837511501df80dc53eeec7599d8afdce193aaa7
Your Name <your.email@example.com>
2025-03-26 14:51:33 +0530

    Initial commit

---
+++ src/main/java/com/skill_sharing_platform/skill_sharing_platform/SkillSharingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.skill_sharing_platform.skill_sharing_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillSharingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillSharingPlatformApplication.class, args);
+	}
+
+}
